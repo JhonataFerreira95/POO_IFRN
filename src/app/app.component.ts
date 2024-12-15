2827d7f41ae0ff4b514b429e5b9e1122e798656b
MauricioIF <mauricio.santos@escolar.ifrn.edu.br>
2024-12-15 11:02:37 -0300

    footer e navbar prontos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FootComponent } from './foot/foot.component';
+import { NavComponent } from "./nav/nav.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavComponent, FootComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
