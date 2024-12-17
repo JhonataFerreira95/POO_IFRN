df5320db74b0960581e10c5bb285e733ca0f30a5
João Guilherme De Lima Oliveira <08263974410@EDUC.GOVRN>
2024-12-17 15:36:40 -0300

    implementação dos atributos e métodos da classe pai

--- src/app/jogo.ts
+++ src/app/jogo.ts
@@ -1,2 +1,16 @@
-export class Jogo {
+export abstract class Jogo {
+    protected _titulo : string;
+    protected _anoPublicacao : Date;
+    protected _fabricante : string;
+    protected _ragras : string [] = []
+
+    constructor(titulo : string, anoPublicacao : Date, fabricante : string){
+        this._titulo = titulo;
+        this._anoPublicacao = anoPublicacao;
+        this._fabricante = fabricante;
+    }
+
+    exibirRegra(): void{
+        this._ragras = []
+    }
 }
